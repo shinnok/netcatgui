fbe7d7c215b27cfaf888eb7b9080b16c9534ae8c
Shinnok <admin@shinnok.com>
2019-02-15 11:05:42 +0000

    distribution: update author and contributors

--- src/util.h
+++ src/util.h
@@ -4,8 +4,7 @@
 #define NCGUI_PROGRAM_NAME      "NetcatGUI"
 #define NCGUI_VERSION           "1.0"
 #define NCGUI_AUTHOR            "Shinnok (Teodor Mircea Ionita)"
-#define NCGUI_CONTACT_EMAIL     "raydenxy [at] gmail.com"
+#define NCGUI_CONTACT_EMAIL     "admin@shinnok.com"
 #define NCGUI_LICENSE           "GNU GPLv2"
-
 
 #endif // UTIL_H
